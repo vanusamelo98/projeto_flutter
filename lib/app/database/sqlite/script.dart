f1c5f353ebaf37d9bd6cc6755ad3bf367715eb88
Vanusamelo98 <vanusamelo982@gmail.com>
2021-10-07 23:54:45 -0300

    exclusão de item

--- lib/app/database/sqlite/script.dart
+++ lib/app/database/sqlite/script.dart
@@ -1,6 +1,6 @@
 final createTable = '''
  CREATE TABLE contact(
-   id INT PRIMARY KEY
+   id INTEGER NOT NULL PRIMARY KEY
    ,nome VARCHAR(200) NOT NULL
    ,telefone CHAR(16) NOT NULL
    ,email VARCHAR(150) NOT NULL
--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -3,9 +3,11 @@
 import 'package:get_it/get_it.dart';
 import 'database/sqlite/dao/contact_dao_impl.dart';
 import 'domain/interfaces/contact_dao.dart';
+import 'domain/services/contact_service.dart';
 
 setupInjection(){
   GetIt getIt = GetIt.I;
 
   getIt.registerSingleton<ContactDAO>(ContactDAOImpl());
+  getIt.registerSingleton<ContactService>(ContactService());
 }
